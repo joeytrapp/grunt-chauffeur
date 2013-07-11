0a94f0d6befb12446a5b81364b12ad125136ed3c
Joey Trapp <joey@loadsys.com>
2013-07-11 09:59:11 -0500

    First functional version

--- tasks/chauffeur.js
+++ tasks/chauffeur.js
@@ -1,25 +1,12 @@
 module.exports = function(grunt) {
-  var express = require('express'),
-      attach = require('../lib/attach'),
+  var chauffeur = require('chauffeur'),
       desc = 'Start a dev server that handles test runner url and proxies requests';
 
   grunt.registerMultiTask('chauffeur', desc, function() {
-    var port = this.data.port || 8000,
-        host = this.data.host || 'localhost',
-        routes = this.data.routes || false,
-        proxy = this.data.proxy || false,
-        testable = this.data.testable || false,
-        staticFiles = this.data.staticFiles || false,
-        done = this.async(),
-        app = express();
+    var done = this.async();
 
-    if (testable) { attach.testable(app, testable); }
-    if (staticFiles) { attach.staticFiles(app, staticFiles); }
-    if (routes) { attach.routes(app, routes); }
-    if (proxy) { attach.proxy(app, proxy); }
-
-    app.listen(port, host, function() {
-      grunt.log.write("Dev server running on port " + port + "\n");
+    chauffeur(this.data, function() {
+      grunt.log.write("Dev server running on port " + this.data.port + "\n");
       done();
     });
   });
