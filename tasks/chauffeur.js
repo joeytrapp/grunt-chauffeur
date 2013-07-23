8765ca8ff7694ac3f11b115116e775b252bdaf5d
Joey Trapp <joey@loadsys.com>
2013-07-23 10:20:03 -0700

    Hooking into the before callback to attach a lockable middleware

--- tasks/chauffeur.js
+++ tasks/chauffeur.js
@@ -1,9 +1,33 @@
 module.exports = function(grunt) {
   var chauffeur = require('chauffeur'),
-      desc = 'Start a dev server that handles test runner url and proxies requests';
+      lockable = require('../lib/lockable'),
+      desc = 'Start a dev server that handles test runner url and proxies requests',
 
   grunt.registerMultiTask('chauffeur', desc, function() {
-    var done = this.async();
+    var lockName = this.name + '.lock', done;
+
+    // Check user supplied data for a name for the lock file
+    // to support multiple lockable chauffeur servers
+    if (this.data.locks) {
+      lockName = this.data.locks.toString();
+    }
+
+    if (this.flags.lock) {
+      lockable.lock(lockName);
+      return;
+    }
+
+    if (this.flags.unlock) {
+      lockable.unlock(lockName);
+      return;
+    }
+
+    // Wait to signify async until app has not returned
+    done = this.async();
+
+    this.data.before = function(app) {
+      lockable.attach(app, lockName);
+    };
 
     chauffeur(this.data, function() {
       grunt.log.write("Dev server running on port " + this.data.port + "\n");
@@ -11,3 +35,4 @@ module.exports = function(grunt) {
     }.bind(this));
   });
 };
+
